5246093f55a8d03dca09beb36c9a4dbcaf09e2e8
Jihunkim95 <darak551@naver.com>
2023-12-21 17:09:08 +0900

    init: ProgressViewDemo

---
+++ ProgressViewDemo/ProgressViewDemo/ProgressViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewDemoApp.swift
+//  ProgressViewDemo
+//
+//  Created by 김지훈 on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
